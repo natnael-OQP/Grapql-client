77735c32bdf345a44e0fd077ffbec58a66f25c78
natnael-OQP <makiya112545@gmail.com>
2022-04-14 22:10:48 +0300

    set up apollo client

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import './App.css'
 
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql,
+} from '@apollo/client'
+
 function App() {
+    const client = new ApolloClient({
+        uri: 'http://localhost:4000/graphql',
+        cache: new InMemoryCache(),
+    })
+
+    const UserList = gql`
+        query getUsers {
+            users {
+                name
+                username
+            }
+        }
+    `
+    const { loading, error, data } = useQuery(UserList)
+    console.log(data)
+
     return (
-        <div className="App">
-            <h1>hello world</h1>
-        </div>
+        <ApolloProvider client={client}>
+            <div className="App">
+                <h1>hello world</h1>
+            </div>
+        </ApolloProvider>
     )
 }
 
